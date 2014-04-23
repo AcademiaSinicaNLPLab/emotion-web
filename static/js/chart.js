c84357bbfae862a64d680321f7016def03f0faad
maxis <maxis1718@gmail.com>
2014-04-23 16:13:27 +0800

    add distribution

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,48 +1,98 @@
-var margin = {top: 30, right: 10, bottom: 50, left: 30},
-    width = 840 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var w = 840;
+var h = 500;
+var width;
+var height;
+var margin = {top: 30, right: 10, bottom: 50, left: 40};
+var x;
+var y;
+var xAxis;
+var yAxis;
+var svg;
 
-// var formatPercent = d3.format(".0%");
+var number = 0;
+var one_bar = 10;
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1, 1);
+function calculate_w_h(w, h, number){
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+  // var c_Height = $(document.body)[0].clientHeight;
+  
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+  // console.log(c_Width);
+  // console.log(c_Height);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    // .tickFormat(formatPercent);
+  var extra = number*one_bar;
 
-var svg = d3.select("#chart-container").append("svg")
+  width = w + extra - margin.left - margin.right;
+  height = h - margin.top - margin.bottom;
+
+  x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1, 1);
+
+  y = d3.scale.linear()
+      .range([height, 0]);
+
+  xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");  
+}
+
+function align() {
+  $('.x').find('.tick').each(function(index, value){
+    var transform = $(this).attr('transform');
+    var ordinate = transform.split('(')[1].split(')')[0].split(',');
+
+    ordinate = $.map(ordinate, function(n){ return parseInt(n); });
+
+    var sink_offset = index%2*15;
+
+    ordinate[1] = ordinate[1] + sink_offset;
+
+    var ordinate_str = $.map(ordinate, function(n){ return n.toString(); }).join(',');
+
+    // console.log(ordinate_str);
+
+    $(this).find('text').attr('transform', 'translate(0,'+sink_offset.toString()+')');
+    var y2 = $(this).find('line').attr('y2');
+    $(this).find('line').attr('y2', parseInt(y2)+index%2*10);
+  });    
+}
+
+function draw(data, number)
+{
+  calculate_w_h(w, h, number);
+  var c_Width = $(document.body)[0].clientWidth;
+
+  var left_offset = c_Width-width < 0 ? 0 : (c_Width-width)/2
+
+
+  var position = {left: left_offset, top: 20};
+  $('#chart-container').css({ 'left': position.left, 'top':position.top })
+
+  // d3.select("#chart-container").remove();
+  $('#chart-container').find('svg').remove();
+  svg = d3.select("#chart-container").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+      .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// d3.tsv("/static/data.tsv", function(error, data) {
-
-$.getJSON('/api/pat/i%20am%20pissed', function(data){
-  // console.log(data);
-
   data.forEach(function(d) {
-    d.val = +d.val;
-  });
+      d.val = +d.val;
+    });
 
-  x.domain(data.map(function(d) { return d.key; }));
-  y.domain([0, d3.max(data, function(d) { return d.val; })]);
+    x.domain(data.map(function(d) { return d.key; }));
+    y.domain([0, d3.max(data, function(d) { return d.val; })]);
 
-  svg.append("g")
+    svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
 
-  svg.append("g")
+    svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
     .append("text")
@@ -52,7 +102,7 @@ $.getJSON('/api/pat/i%20am%20pissed', function(data){
       .style("text-anchor", "end")
       .text("Frequency");
 
-  svg.selectAll(".bar")
+    svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
@@ -61,38 +111,7 @@ $.getJSON('/api/pat/i%20am%20pissed', function(data){
       .attr("y", function(d) { return y(d.val); })
       .attr("height", function(d) { return height - y(d.val); });
 
-  d3.select("input").on("change", change);
-
-  // var sortTimeout = setTimeout(function() {
-  //   d3.select("input").property("checked", true).each(change);
-  // }, 2000);
-
-  function align() {
-    $('.x').find('.tick').each(function(index, value){
-      var transform = $(this).attr('transform');
-      var ordinate = transform.split('(')[1].split(')')[0].split(',');
-
-      ordinate = $.map(ordinate, function(n){ return parseInt(n); });
-
-      var sink_offset = index%2*15;
-
-      ordinate[1] = ordinate[1] + sink_offset;
-
-      var ordinate_str = $.map(ordinate, function(n){ return n.toString(); }).join(',');
-
-      // console.log(ordinate_str);
-
-      $(this).find('text').attr('transform', 'translate(0,'+sink_offset.toString()+')');
-      var y2 = $(this).find('line').attr('y2');
-      $(this).find('line').attr('y2', parseInt(y2)+index%2*10);
-    });    
-  }
-
-  align();
-
-  function change() {
-    // clearTimeout(sortTimeout);
-
+  d3.select("input").on("change", function(){
     // Copy-on-write since tweens are evaluated after a delay.
     var x0 = x.domain(data.sort(this.checked
         ? function(a, b) { return b.val - a.val; }
@@ -113,5 +132,10 @@ $.getJSON('/api/pat/i%20am%20pissed', function(data){
         .delay(delay);
 
     align();
-  } 
-});
+
+    var checkbox_wrap = $('#chart-container').find('.checkbox-wrap');
+    checkbox_wrap.toggleClass('checked');   
+  });
+
+  align();
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	events();
 });
+
+var data = [];
+
 function events(){
 	$('.emotion-button').click(function(){
 		location.href = "../" + $(this)[0].innerText;
@@ -13,4 +16,34 @@ function events(){
 	$('.sent').hover(function(){
 		$(this).siblings('.pat').toggleClass('hide');
 	});	
+
+	$('.pat').click(function(){
+		var pattern = $(this).text();
+		$('#chart-container').find('.pattern').text(pattern);
+
+
+		$.getJSON('/api/pat/'+pattern.toLowerCase(), function(data){
+
+			var number = 0;
+
+			console.log(data.length);
+
+			if(data.length == 0) {
+				return false;
+			}
+			draw(data, data.length);
+			$('#chart-container').toggleClass('hide');
+			$('.mask').toggleClass('hide');
+		});
+
+		
+	});
+	$('.close').click(function(){
+		$(this).parents('.container').toggleClass('hide');
+		$('.mask').toggleClass('hide');
+	});
+	$('.mask').click(function(){
+		$('.close').click();
+	});
 }
+
