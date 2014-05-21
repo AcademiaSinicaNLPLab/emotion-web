5abfba0675b4ca0b9da23059ae81e83792c38b95
maxis <maxis1718@gmail.com>
2014-05-21 16:00:25 +0800

    use setting_id and svm_param to select matrix

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -3,7 +3,7 @@
 # import sys, pymongo, color
 
 from collections import defaultdict, Counter
-import json, sys
+import json, sys, os, re, color
 
 ### input
 # file 1: gold		e.g., gold.txt
@@ -15,28 +15,77 @@ import json, sys
 # data_folder_name = 'feelit-data'
 # data_folder_root = os.path.join(os.getcwd(), data_folder_name)
 
-path_to_answer = '../tmp/0.out'
-path_to_gold = '../tmp/gold.txt'
+# path_to_answer = '../tmp/0.out'
+# path_to_gold = '../tmp/gold.txt'
+
+path_to_gold = None
+path_to_out  = None
 
 answers = []
 golds = []
 labels = {}
 
-# print >> sys.stderr, '[path] [confusion_matrix.py] current working directory:', os.getcwd()
+### default path setting
+external_search = '../emotion-detection-modules/tmp'
+internal_search = 'tmp'
 
+setting_id = '537b00e33681df445d93d57e'
+svm_param = 'c9r2t1'
+
+def search_files():
+	global internal_search, external_search, setting_id, svm_param
+	# search .out and .gold.txt
+	## search internal
+	out_path = os.path.join(internal_search, '.'.join([setting_id, svm_param, 'out']))
+	gold_path = os.path.join(internal_search, '.'.join([setting_id, 'gold', 'txt']))
+
+
+
+	found_out = os.path.exists(out_path)
+	found_gold = os.path.exists(gold_path)
+	## search external
+	if not found_out:
+		out_path = os.path.join(external_search, '.'.join([setting_id, svm_param, 'out']))
+		found_out = os.path.exists(out_path)
+	if not found_gold:
+		gold_path = os.path.join(external_search, '.'.join([setting_id, 'gold', 'txt']))
+		found_gold = os.path.exists(gold_path)
+	
+
+	print "out_path:",out_path
+	print "gold_path:",gold_path
+	print 
+
+	if found_out and found_gold:
+		return {'out': out_path, 'gold': gold_path}
+	else:
+		return False
+
+# def list_all():
+# 	## list external
+# 	fns = [x for x in os.listdir(external_search) if re.match(r'^[0-9a-z]{24}', x)]
+
+# 	fns
+
+# 	os.listdir(external_search)
+
+
+	pass
 
 def load_data():
 	global answers, golds, labels
 
-	# print >> sys.stderr, '[path] [confusion_matrix.py] current working directory:', os.getcwd()
-	# print >> sys.stderr, '[path] [confusion_matrix.py] data root:', data_folder_root
+	paths = search_files()
 
-	print >> sys.stderr, '[path] [confusion_matrix.py] path_to_answer:',path_to_answer
-	print >> sys.stderr, '[path] [confusion_matrix.py] path_to_gold:',path_to_gold
+	if paths:
+		print >> sys.stderr, '[path] [confusion_matrix.py] path for out:', paths['out']
+		print >> sys.stderr, '[path] [confusion_matrix.py] path for gold:',paths['gold']		
+		answers = [line.strip().split('\t')[0] for line in open(paths['out'])]
+		golds = [line.strip().split('\t')[0] for line in open(paths['gold'])]
+		labels = { line.strip().split('\t')[0]:line.strip().split('\t')[-1] for line in open(paths['gold']) }		
+	else:
+		print >> sys.stderr, color.render('[erorr] [confusion_matrix.py] cannot find the files.', 'r')
 
-	answers = [line.strip().split('\t')[0] for line in open(path_to_answer)]
-	golds = [line.strip().split('\t')[0] for line in open(path_to_gold)]
-	labels = { line.strip().split('\t')[0]:line.strip().split('\t')[-1] for line in open(path_to_gold) }
 
 def generate():
 	global answers, golds, labels
@@ -59,6 +108,7 @@ def generate():
 	return matrix
 
 if __name__ == '__main__':
+
 	for gold_label in sorted(matrix.keys()):
 		# sum([x[1] for x in matrix[gold_label].items()])
 		print gold_label, sorted(matrix[gold_label].items(), key=lambda x:x[1], reverse=True)
